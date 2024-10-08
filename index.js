635f853ca119f3e91d2fc2a3e46c4070dd9d5a63
Andrew Taing <person12212@gmail.com>
2024-10-08 06:30:16 -0500

    Added comments

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const express = require('express');
+const express = require('express'); //we're using express.js
 const app = express();
-app.use(express.static('public'));
-app.use(express.json());
+app.use(express.static('public')); //tell the app that the frontend is in the 'public' folder so it knows what to display
+app.use(express.json()); //tell the app that we will be passing information in JSON format
 
+//GET REQUEST
+//used to pass information from the server to the frontend
+//for the login page it's not used
+//i've read that you can just use POST instead of GET and it's fine most of the time so it's probably ok for anything we're doing
+//but idk
 app.get('/', (req, res) => {
     res.status(200).send('good');
 });
 
+//POST REQUEST
+//used to submit forms and modify server-side data
 app.post('/', (req, res) => {
-    const {parcel} = req.body;
+    const {parcel} = req.body; //set parcel to be the request information
     console.log(parcel.user);
-    if (parcel.user == "admin" && parcel.pass == "admin") {
+    if (parcel.user == "admin" && parcel.pass == "admin") { //authentication; send response to frontend afterwards
         res.status(200).json({auth: 'correct'});
     }
     else {
@@ -18,4 +25,4 @@ app.post('/', (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log('App available on http://localhost:3000'));
+app.listen(3000, () => console.log('App available on http://localhost:3000')); //tell the app to listen on port 3000
--- public/login/login.js
+++ public/login/login.js
@@ -2,19 +2,41 @@ function gotoLink(link) {
     window.location.href = link.value;
 }
 
-//this all should be backend later
-function auth(event) {
-    var user = document.getElementById("username").value;
-    var pw = document.getElementById("password").value;
-    if (user == "admin" && pw == "admin") {
-        //redirect to home page
-        window.location.href = "./home/home.html";
-    }
-    else {
-        document.querySelectorAll(".form_message-error").forEach(x => x.style.display = "block");
-    }
-    event.preventDefault();
-}
+const baseURL = 'http://localhost:3000/'; //the URL of the server
 
-const login_ele = document.querySelector(".login_form");
-login_ele.addEventListener("submit", auth, false);
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const login_ele = document.getElementById('login_form');
+
+login_ele.addEventListener('submit', authenticate);
+
+async function authenticate(e) { 
+    e.preventDefault(); //prevent submit from doing its default behavior because it will normally refresh the page
+    var obj = { //object to store the user + pw
+        user: username.value,
+        pass: password.value
+    };
+
+    const res = await fetch(baseURL, { //https://www.youtube.com/watch?v=5TxF9PQaq4U&list=LL&index=3
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            parcel: obj
+        })
+    }).then((response) => { //he doesn't go over this in the video
+        //basically the fetch() function returns a "response stream"
+        //this is NOT the same as a JSON. we have to return response.json() to be able to access the JSON
+        return response.json();
+    }).then((json) => { //here we can use the JSON
+        //we can also do this with text, so we could've returned response.text() previously
+        //and then done .then((text)) => {} to work with the raw text
+        if (json.auth == "correct") { 
+            window.location.href = "./home/home.html";
+        }
+        else {
+            document.querySelectorAll(".form_message-error").forEach(x => x.style.display = "block"); //display error message
+        }
+    })
+}

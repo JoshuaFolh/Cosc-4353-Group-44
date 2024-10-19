057cff8775e449e9911b0906ff674eff34b936c5
Joshua Folh <joshfolh@gmail.com>
2024-10-18 21:59:39 -0500

    JS Heavy approach

--- public/home/profile/profile.js
+++ public/home/profile/profile.js
@@ -2,21 +2,42 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 
-const app = express();
+reg_ele.addEventListener('submit', profile_change);
 
-// Middleware to parse form data
-app.use(bodyParser.urlencoded({ extended: true }));
+async function profile_change(e) { 
+    e.preventDefault(); 
+    var obj = { 
+        fullName: fullName.value,
+        address1: address1.value,
+        address2: address2.value,
+        city: city.value,
+        zipCode: zipCode.value,
+        preference: preference.value,
+        stateSel: stateSel.value,
+        skills: skills.value, // This will be an array if multiple values are selected
+        availability: availability.value
+    };
 
-// Initialize multer for handling multipart/form-data
-const upload = multer();
-
-// Serve the profile.html file
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/profile.html');
-});
-
-// Start the server
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+    const res = await fetch(baseURL + 'profile', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            parcel: obj
+        })
+    }).then((response) => {
+        return response.json();
+    }).then((json) => { 
+        if (json.auth == "valid") { 
+            var sessionTimeout = 1; //hour
+            var loginDuration = new Date();
+            loginDuration.setTime(loginDuration.getTime() + (sessionTimeout*60*60*1000));
+            document.cookie = "profile_change=true; " + "expires=" + loginDuration.toGMTString() + "; path=/";
+            window.location.href = 'public\home\profile\profile.html';
+        }
+        else {
+            document.querySelectorAll(".form_message-error").forEach(x => x.style.display = "block"); //display error message
+        }
+    })
+}

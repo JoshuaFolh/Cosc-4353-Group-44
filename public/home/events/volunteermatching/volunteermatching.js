f44de26f0f2d16cda9a049dc45dc15b6a9cc8989
ataing01 <ataing01@gmail.com>
2024-10-18 20:32:06 -0500

    Added volunteer matching backend

--- public/home/events/volunteermatching/volunteermatching.js
+++ public/home/events/volunteermatching/volunteermatching.js
@@ -1,9 +1,53 @@
+const baseURL = 'http://localhost:3000/';
+
+//not in use
 function addVolunteer() {
     const volunteer_rect = document.querySelector(".volunteer_rect");
     volunteer_rect.innerHTML += `<div class="volunteer_item">Volunteer Details</div>`;
 }
 
-function addEvent() {
+function addEvent(string) {
     const event_rect = document.querySelector(".event_rect");
-    event_rect.innerHTML += `<div class="event_item">Event Details</div>`;
+    event_rect.innerHTML += '<div class="event_item">' + string + '</div>';
+}
+
+function clearEvents() {
+    const event_rect = document.querySelector(".event_rect");
+    event_rect.innerHTML = "";
+}
+
+const volunteer_ele = document.querySelector(".volunteer_lookup");
+volunteer_ele.addEventListener("submit", findEventsForVolunteer);
+
+async function findEventsForVolunteer(e) { 
+    clearEvents();
+    e.preventDefault(); 
+    var obj = { 
+        user: username.value,
+    };
+
+    const res = await fetch(baseURL + 'find_events', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            parcel: obj
+        })
+    }).then((response) => {
+        return response.json();
+    }).then((json) => { 
+        if (json.status == "no such user found!") {
+            console.log("joever");
+        }
+        else {
+            console.log(json);
+            for (const i in json) {
+                addEvent(
+                    json[i].name + '<br>' +
+                    json[i].skills + '<br>'
+                );
+            }
+        }
+    })
 }

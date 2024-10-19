c6708043cb0d5a0c9b9f8ee7987ab1e10c08472c
ataing01 <ataing01@gmail.com>
2024-10-19 18:21:21 -0500

    Disabled login cookie

--- public/home/home.js
+++ public/home/home.js
@@ -1,3 +1,6 @@
+//this will have to be rolled out to all pages probably
+//automatically kick the user to login if they didn't log in
+/*
 if (document.cookie.indexOf("logged_in=true") == -1) {
     window.location.href = "../../index.html";
-}
+}*/

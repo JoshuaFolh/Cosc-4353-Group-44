efe796978c971fcd3caa41eda8f03a71a9ea7eb2
Joshua Folh <joshfolh@gmail.com>
2024-11-01 22:41:59 -0500

    Work
    Removed the garbage app.ues section that causes errors.

--- index.js
+++ index.js
@@ -10,11 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(session({
-  secret: 'your-secret-key',
-  resave: false,
-  saveUninitialized: false
-}));
+
 app.use(express.static('public')); 
 
 app.get('/', (req, res) => {

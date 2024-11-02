f334d9bf4fb43039d3eca3ec241fad33dfac5d37
ataing01 <ataing01@gmail.com>
2024-11-01 22:01:42 -0500

    merged userDetails with userCred

--- models/userCredModel.js
+++ models/userCredModel.js
@@ -1,5 +1,35 @@
 const mongoose = require('mongoose');
 
+const userDetailsSchema = new mongoose.Schema({
+    fullname: {
+        type: String
+    },
+    address1: {
+        type: String
+    },
+    address2: {
+        type: String
+    },
+    city: {
+        type: String
+    },
+    zipcode: {
+        type: String
+    },
+    preference: {
+        type: String
+    },
+    state: {
+        type: String
+    },
+    skills: {
+        type: [String]
+    },
+    availability: {
+        type: String
+    },
+});
+
 const userCredSchema = new mongoose.Schema({
     username: {
         type: String,
@@ -13,16 +43,23 @@ const userCredSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    details: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'userDetails'
-    }],
+    details:
+        userDetailsSchema
+    ,
     isAdmin: {
         type: Boolean,
         required: true
+    },
+    notifs: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'notifs'
+    }],
+    volHistory: {
+        type: [String]
     }
 });
 
+const userDetails = new mongoose.model("userDetails", userDetailsSchema);
 const userCollection = new mongoose.model("userCollection", userCredSchema);
 
 module.exports = userCollection;
